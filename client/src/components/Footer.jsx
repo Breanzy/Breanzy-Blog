40f0bd958025bf36941c3231d31521dd1363a361
Brean Julius Carbonilla <julius.carbonilla@gmail.com>
2025-07-01 22:07:01 +1000

    just let me do this for once please. let me ball. tomorrow i hope

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -33,7 +33,7 @@ export default function FooterCom() {
                                     target="_blank"
                                     rel="noopener noreferrer"
                                 >
-                                    Facebook ahahakuygft
+                                    Facebook ahahakuygftasdf
                                 </Footer.Link>
                                 <Footer.Link
                                     href="/about"
