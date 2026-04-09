d2e8e9a503ced8d1cc5213d9e9b5b10f0b47f267
Brean Julius Carbonilla <julius.carbonilla@gmail.com>
2026-04-09 23:50:42 +1000

    Adapt Express server for Vercel serverless

--- api/index.js
+++ api/index.js
@@ -1,34 +1,27 @@
 import express from "express";
-import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import { connectDB } from "./utils/db.js";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 import postRoutes from "./routes/post.route.js";
 import commentRoutes from "./routes/comment.route.js";
 import projectRoutes from "./routes/project.route.js";
-import cookieParser from "cookie-parser";
-import path from "path";
 
 dotenv.config();
 
-mongoose
-    .connect(process.env.MONGO)
-    .then(() => {
-        console.log("Connected to MongoDB");
-    })
-    .catch((err) => {
-        console.error(err);
-    });
-
-const __dirname = path.resolve();
-
 const app = express();
-
 app.use(express.json());
 app.use(cookieParser());
 
-app.listen(3000, () => {
-    console.log("listening on on port 3000");
+// Connect to MongoDB before each request (cached after first cold start)
+app.use(async (_req, _res, next) => {
+    try {
+        await connectDB();
+        next();
+    } catch (err) {
+        next(err);
+    }
 });
 
 app.use("/api/user", userRoutes);
@@ -37,17 +30,11 @@ app.use("/api/post", postRoutes);
 app.use("/api/comment", commentRoutes);
 app.use("/api/project", projectRoutes);
 
-app.use(express.static(path.join(__dirname, "/client/dist")));
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
-});
-
-app.use((err, req, res, next) => {
+// Global error handler
+app.use((err, _req, res, _next) => {
     const statusCode = err.statusCode || 500;
     const message = err.message || "Internal Server Error";
-    res.status(statusCode).json({
-        success: false,
-        statusCode,
-        message,
-    });
+    res.status(statusCode).json({ success: false, statusCode, message });
 });
+
+export default app;
