85a17d95d6f98125d846fc6cb5b23d487310cd38
Brean Julius Carbonilla <julius.carbonilla@gmail.com>
2026-04-15 00:02:05 +1000

    feat: add loading spinners for async page routes

---
+++ app/blog/[slug]/loading.tsx
@@ -0,0 +1,7 @@
+export default function PostLoading() {
+    return (
+        <div className="bg-black min-h-screen flex items-center justify-center">
+            <div className="w-10 h-10 border-4 border-neutral-700 border-t-blue-500 rounded-full animate-spin" />
+        </div>
+    );
+}
